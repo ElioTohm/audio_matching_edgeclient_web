c73cab87c04d2f62839c08c8f3cee9b2d5a3a7b7
Elio Tohme <eliotohme@gmail.com>
2017-03-15 16:47:18 +0200

    bug fix

--- flaskr/flaskr/static/js/connect.js
+++ flaskr/flaskr/static/js/connect.js
@@ -2,5 +2,5 @@ document.getElementById("Connect").className = "active";
 document.getElementById("Status").className = "";
 
 $('.btn-success').on( 'click',function () {
-	$('#SSID').val() = $(this).attr('id');
+	$('#SSID').val($(this).attr('id'));
 });
