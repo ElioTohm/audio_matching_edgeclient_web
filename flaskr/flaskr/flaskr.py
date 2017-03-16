5a9a337f48c94c67b6a730f27a8dc4f65b26c30e
Elio Tohme <eliotohme@gmail.com>
2017-03-16 12:04:57 +0200

    bug fix

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -25,7 +25,7 @@ app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='PM@#12345'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
