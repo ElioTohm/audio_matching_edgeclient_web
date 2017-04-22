4e762cd8b13f2bd21ab040aa18ea8f7c8fcb86e2
Elio Tohme <eliotohme@gmail.com>
2017-04-22 13:02:02 +0300

    bug fix

--- client_record.py
+++ client_record.py
@@ -22,37 +22,37 @@ with open('/home/conf.json') as json_data_file:
     # read json info from file
     DATA = json.load(json_data_file)
 
-if DATA['registered']:
+    if DATA['registered']:
 
-    # start record
-    subprocess.call('arecord -d 30 -f dat -c 1 /home/records/c_' +
-                    str(DATA['registered']) + '_'+str(NOW) + '.wav',
-                    shell=True)
-    subprocess.call('lame -r -s 48 -m m -b 64 /home/records/c_' +
-                    str(DATA['registered'])+'_' + str(NOW) +
-                    '.wav /home/records/c_' +
-                    str(DATA['registered']) + '_' + str(NOW)+'.mp3',
-                    shell=True)
-    subprocess.call('rm /home/records/c_' + str(DATA['registered']) +
-                    '_' + str(NOW) +
-                    '.wav', shell=True)
+        # start record
+        subprocess.call('arecord -d 30 -f dat -c 1 /home/records/c_' +
+                        str(DATA['registered']) + '_'+str(NOW) + '.wav',
+                        shell=True)
+        subprocess.call('lame -r -s 48 -m m -b 64 /home/records/c_' +
+                        str(DATA['registered'])+'_' + str(NOW) +
+                        '.wav /home/records/c_' +
+                        str(DATA['registered']) + '_' + str(NOW)+'.mp3',
+                        shell=True)
+        subprocess.call('rm /home/records/c_' + str(DATA['registered']) +
+                        '_' + str(NOW) +
+                        '.wav', shell=True)
 
-    time.sleep(120)
+        time.sleep(120)
 
-    # add file
-    FILES = []
+        # add file
+        FILES = []
 
-    for filerecorded in os.listdir('/home/records/'):
-        if filerecorded.endswith('.mp3'):
-            FILES.append(('client_record', open('/home/records/' + filerecorded, 'rb')))
-
-    #  send file to server by post request
-    R = requests.post(URL,
-                      files=FILES,
-                      auth=('elio', '201092elio'),
-                      timeout=15)
-
-    if R.status_code == 200:
         for filerecorded in os.listdir('/home/records/'):
             if filerecorded.endswith('.mp3'):
-                os.unlink('/home/records/' + filerecorded)
+                FILES.append(('client_record', open('/home/records/' + filerecorded, 'rb')))
+
+        #  send file to server by post request
+        R = requests.post(URL,
+                          files=FILES,
+                          auth=('elio', '201092elio'),
+                          timeout=15)
+
+        if R.status_code == 200:
+            for filerecorded in os.listdir('/home/records/'):
+                if filerecorded.endswith('.mp3'):
+                    os.unlink('/home/records/' + filerecorded)
