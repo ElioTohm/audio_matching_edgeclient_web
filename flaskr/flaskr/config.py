ed27a820a84d9bf4d29b67b776bf9000bc3b86a3
Elio Tohme <eliotohme@gmail.com>
2017-05-04 17:45:22 +0300

    refactoring

--- flaskr/flaskr/config.py
+++ flaskr/flaskr/config.py
@@ -5,7 +5,7 @@ Config file for flask
 PORT = 8080
 SUB_ADMIN = 'Admin'
 SUB_USER = 'Client'
-URL = 'localhost'
+URL = '0.0.0.0'
 USER_NAME = 'pahopmclient'
 PASSWORD = 'xms@pmclient#12345'
 GIT_REPO = 'https://elio_tohme:33D22ji1201092@bitbucket.org/xmspro/clientwebadmin.git'
