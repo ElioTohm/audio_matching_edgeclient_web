b527e3c38089b50b55b19eddd093d9aaa74cae8b
Elio Tohme <eliotohme@gmail.com>
2017-05-18 14:59:52 +0300

    bug fix and refactoring / now client online send history form 1 to 4 am

--- client_record.py
+++ client_record.py
@@ -8,43 +8,48 @@ import subprocess
 import time
 import json
 import os
+import time as TIME
+from datetime import datetime, time
 import requests
 import config_main
 
 # add route
-subprocess.call(config_main.ROUTE_EXEC, shell=True)
+# subprocess.call(config_main.ROUTE_EXEC, shell=True)
 
 # take time
-NOW = int(time.time())
-
+NOW = int(TIME.time())
+CURRENT_TIME = datetime.now().time()
+print NOW
 # open json file to read client id
 with open('/data/conf.json') as json_data_file:
     # read json info from file
     DATA = json.load(json_data_file)
 
     if DATA['registered']:
+        REC_NAME = "c_{}_{}".format(str(DATA['registered']), str(NOW))
 
         # start record
-        subprocess.call('arecord -d 30 -f dat -c 1 /home/records/c_' +
-                        str(DATA['registered']) + '_'+str(NOW) + '.wav',
+        subprocess.call("arecord -d 30 -f dat -c 1 /home/records/{}.wav".format(REC_NAME),
                         shell=True)
-        subprocess.call('lame -r -s 48 -m m -b 64 /home/records/c_' +
-                        str(DATA['registered'])+'_' + str(NOW) +
-                        '.wav /home/records/c_' +
-                        str(DATA['registered']) + '_' + str(NOW)+'.mp3',
+        subprocess.call("lame -r -s 48 -m m -b 64 /home/records/{}.wav /home/records/{}.mp3".format(REC_NAME, REC_NAME),
                         shell=True)
-        subprocess.call('rm /home/records/c_' + str(DATA['registered']) +
-                        '_' + str(NOW) +
-                        '.wav', shell=True)
+        subprocess.call("rm /home/records/{}.wav".format(REC_NAME), shell=True)
 
-        time.sleep(10)
+        TIME.sleep(10)
 
         # add file
         FILES = []
+        UNLINK_ALL = None
 
-        for filerecorded in os.listdir('/home/records/'):
-            if filerecorded.endswith('.mp3'):
-                FILES.append(('client_record', open('/home/records/' + filerecorded, 'rb')))
+        if CURRENT_TIME >= time(1, 00) and CURRENT_TIME <= time(4, 00):
+            UNLINK_ALL = True
+            for filerecorded in os.listdir('/home/records/'):
+                if filerecorded.endswith('.mp3'):
+                    FILES.append(('client_record', open('/home/records/' + filerecorded, 'rb')))
+        else:
+            UNLINK_ALL = False
+            FILES.append(('client_record', open("/home/records/{}.mp3".format(REC_NAME), 'rb')))
+
 
         #  send file to server by post request
         R = requests.post(config_main.URL,
@@ -53,6 +58,9 @@ with open('/data/conf.json') as json_data_file:
                           timeout=15)
 
         if R.status_code == 200:
-            for filerecorded in os.listdir('/home/records/'):
-                if filerecorded.endswith('.mp3'):
-                    os.unlink('/home/records/' + filerecorded)
+            if UNLINK_ALL:
+                for filerecorded in os.listdir('/home/records/'):
+                    if filerecorded.endswith('.mp3'):
+                        os.unlink('/home/records/{}'.format(filerecorded))
+            else:
+                os.unlink("/home/records/{}.mp3".format(REC_NAME))
